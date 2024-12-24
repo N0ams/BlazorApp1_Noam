9fefb7216e202f876f40002524b840031f534495
N0am <shelkovitznoam@gmail.com>
2024-12-24 16:21:58 +0200

    Add project files.

---
+++ BlazorApp1_Noam/Components/Pages/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp1_Noam.Components.Pages
+{
+    public class Person
+    {
+        private string firstName;
+
+        private string lastName;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+    }
+}
